5260243c1a8f27a4b93d95f095f5be2edc5adabb
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2021-04-24 23:30:40 -0500

    Combined info and calculated age

--- src/model/Dog.java
+++ src/model/Dog.java
@@ -1,15 +1,70 @@
 package model;
 
+import java.util.Date;
+
 import processing.core.PApplet;
 
 public class Dog {
 	
+	private int id;
+	private String name;
+	private String breed;
+	private String birthDate;
+	private long age;
+	
 	private PApplet app;
 
-	public Dog(PApplet app) {
+	public Dog(int id, String name, String breed, String birthDate, long age, PApplet app) {
 		
+		this.id = id;
+		this.name = name;
+		this.breed = breed;
+		this.birthDate = birthDate;
+		this.age = age;
 		this.app = app;
 		
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBreed() {
+		return breed;
+	}
+
+	public void setBreed(String breed) {
+		this.breed = breed;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public long getAge() {
+		return age;
+	}
+
+	public void setAge(long age) {
+		this.age = age;
+	}
+	
+	
 
 }
--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,26 +1,29 @@
 package model;
 
+import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 
 import processing.core.PApplet;
 
 public class Logic {
-
+	private Dog dog;
 	private PApplet app;
 	private String[] txt1;
 	private String[] txt2;
 	private String[] array1;
 	private String[] array2;
-	private LinkedList<Dog> dogList;
-	private ArrayList<String> firstList;
+	private LinkedList<Dog> listDog;
 
 	public Logic(PApplet app) {
 
 		this.app = app;
-		dogList = new LinkedList<Dog>();
-		firstList = new ArrayList<String>();
+		listDog = new LinkedList<Dog>();
 		loadTxt();
+		// combineInfo();
 
 	}
 
@@ -29,33 +32,53 @@ public class Logic {
 		txt1 = app.loadStrings("./data/imports/TXT1.txt");
 		txt2 = app.loadStrings("./data/imports/TXT2.txt");
 		txtToWords();
-		CombineInfo();
 
 	}
 
 	private void txtToWords() {
+
 		for (int i = 0; i < txt1.length; i++) {
 			array1 = txt1[i].split(" ");
-			for (int j = 0; j < array1.length; j++) {
-				System.out.println(array1[j]);
-			}
+			int id = Integer.parseInt(array1[0]);
+			String name = array1[1];
+			// System.out.println(id);
+			// System.out.println(name);
 
+			for (int j = 0; j < txt2.length; j++) {
+				array2 = txt2[j].split(" ");
+				if (id == Integer.parseInt(array2[0])) {
+
+					String breed = array2[1];
+					String birthDate = array2[2];
+					//System.out.println(breed);
+					// System.out.println(birthDate);
+					// Date birthDate1;
+
+					SimpleDateFormat birthDate1 = new SimpleDateFormat("dd-mm-yyyy");
+					Date date;
+					try {
+						date = birthDate1.parse(birthDate);
+						long ms = System.currentTimeMillis() - date.getTime();
+						long age = (long) ((long) ms / (1000.0 * 60 * 60 * 24 * 365));
+						//System.out.println(birthDate + age);
+						combineInfo(id, name, breed, birthDate, age);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				
+				
+			}
+				
 		}
 
-		for (int i = 0; i < txt2.length; i++) {
-			array2 = txt2[i].split(" ");
-			for (int j = 0; j < array2.length; j++) {
-				System.out.println(array2[j]);
-			}
-		}
 	}
 
-	private void CombineInfo() {
-
-		
-
-		
-
+	private void combineInfo(int id, String name, String breed, String birthDate, long age) {
+		listDog.add(new Dog(id, name, breed, birthDate, age, app));
+		System.out.println(listDog.get(0).getAge());
 	}
 
 }
